468fe8191dc20c06e634688768fff160f0cb68f5
nhatminhtrieu <21127112@student.hcmus.edu.vn>
2023-12-31 15:36:20 +0700

    AMO-10-test complete UI & CRUD

--- routes/wardRoutes.js
+++ routes/wardRoutes.js
@@ -7,87 +7,99 @@ const router = express.Router();
 const wardService = new WardService();
 const districtService = new DistrictService();
 
-// Read all as district officer
-router
-    .get('/', async (req, res) => {
-        const result = await wardService.getAllWards(); // get all wards for admin
-        if (!result) {
-            return res.status(404).send("Ward not found");
-        }
-        return res.render('vwWard/wards', { layout: 'admin.hbs', ward: result });
-    })
+function paginate(array, page, limit) {
+    const skip = (page - 1) * limit;
+    const items = array.slice(skip, skip + limit);
+    const pages = Array.from({ length: Math.ceil(array.length / limit) }, (_, i) => ({
+        number: i + 1,
+        isCurrent: i + 1 === page,
+    }));
 
-// Create
-router
-    // .get('/create', async (req, res) => {
-    //     try {
-    //         const districts = await districtService.getAllDistricts();
-    //         res.render('vwWard/createWard', { layout: 'admin.hbs', districts: districts });
-    //     } catch (error) {
-    //         console.error(error);
-    //         res.status(500).send("An error occurred while getting districts");
-    //     }
-    // })
-    .post('/create', async (req, res) => {
-        try {
-            const { wardName, districtID } = req.body;
-            const result = await wardService.addWard({ ward: wardName, district: districtID });
-            res.redirect('/wards');
-        } catch (error) {
-            console.error(error);
-            res.status(500).send("An error occurred while creating the ward");
-        }
-    })
+    return { items, pages };
+}
+
+// Read all as district officer
+router.get('/', async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = 10;
+    const allWards = await wardService.getAllWards();
+    const { items: wards, pages } = paginate(allWards, page, limit);
+
+    return res.render('vwAdmin/wards', {
+        layout: 'admin',
+        ward: wards,
+        isWardList: true,
+        pages: pages
+    });
+});
 
 // /wards/658955201284436f7935f8d2
-router
-    .get('/', async (req, res) => {
-        try {
-            const { id } = req.query;
-            const result = await wardService.getAllWardsByDistrict(id);
-            if (!result) {
-                return res.status(404).send("Ward not found");
-            }
-            return res.render('vwWard/wards', { layout: 'admin.hbs', ward: result });
-        } catch (error) {
-            console.error(error);
-            res.status(500).send("An error occurred");
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const page = parseInt(req.query.page) || 1;
+        const limit = 5;
+        const allWards = await wardService.getAllWardsByDistrict(id);
+        const { items: wards, pages } = paginate(allWards, page, limit);
+        const district = await districtService.getDistrictById(id);
+
+        if (!wards.length) {
+            return res.status(404).send("Ward not found");
         }
-    })
+
+        return res.render('vwAdmin/wards', {
+            layout: 'admin',
+            ward: wards,
+            district: district,
+            isWardList: false,
+            pages: pages
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("An error occurred");
+    }
+});
+
+// Create
+router.post('/create', async (req, res) => {
+    try {
+        const { ward, district } = req.body;
+        const result = await wardService.addWard({ ward: ward, district: district });
+        if (result)
+            return res.json({ success: true });
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false });
+    }
+})
 
 // Update
-router
-    .get("/update", async (req, res) => {
-        try {
-            const { id, districtID } = req.query;
-            const ward = await wardService.getWardById(id);
-            const district = await districtService.getDistrictById(districtID);
-            res.render('vwWard/editWard', { layout: 'admin.hbs', ward: ward, district: district });
-        } catch (error) {
-            console.error(error);
-            res.status(500).send("An error occurred while getting the ward or district");
-        }
-    })
-    .post("/update", async (req, res) => {
-        try {
-            const { id, districtID } = req.query;
-            const { newName } = req.body;
-            const result = await wardService.updateWard(id, districtID, newName);
-            res.redirect('/wards');
-        } catch (error) {
-            console.error(error);
-            res.status(500).send("An error occurred while updating the ward");
-        }
-    })
+// /wards/update?id=id&districtID=districtID
+router.post("/update", async (req, res) => {
+    try {
+        const { id, districtID } = req.query;
+        const { ward } = req.body;
 
+        const result = await wardService.updateWard(id, districtID, ward);
+        if (result)
+            return res.json({ success: true });
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false });
+    }
+})
+
+// Delete
+// /wards/delete?id=id&districtID=districtID
 router.post('/delete', async (req, res) => {
     try {
         const { id, districtID } = req.query;
-        await wardService.deleteWard(id, districtID);
-        res.redirect('/wards');
+        const result = await wardService.deleteWard(id, districtID)
+        if (result)
+            return res.json({ success: true });
     } catch (error) {
         console.error(error);
-        res.status(500).send("An error occurred while deleting the ward");
+        res.json({ success: false });
     }
 })
 

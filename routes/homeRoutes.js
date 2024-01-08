29ef90ac846fab65b13acfac939ced330bbc6eb8
ducviet5138 <ducviet5138@gmail.com>
2024-01-08 23:22:10 +0700

    AMO-16 Render locations base on role

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,10 +1,36 @@
 import express from "express";
 import LocationService from "../services/LocationService.js";
+import mongoose from 'mongoose';
 const router = express.Router();
 
 router.get("/", async (req, res) => {
 	const service = new LocationService();
-	const locations = await service.findAllLocations();
+	let locations = [];
+
+	console.log(req.session.authUser)
+	// console.log(typeof req.session.role)
+
+	// Role: Department Officer
+	if (req.session.authUser.role === 3) {
+		locations = await service.findAllLocations();
+	}
+	// Role: District Officer
+	if (req.session.authUser.role === 2) {
+		locations = await service.find({ 
+			"area.district": new mongoose.Types.ObjectId(req.session.authUser.district)
+		});
+	}
+	// Role: Ward Officer
+	else if (req.session.authUser.role === 1) {
+		locations = await service.find({
+			area: { 
+				district: new mongoose.Types.ObjectId(req.session.authUser.district), 
+				ward: new mongoose.Types.ObjectId(req.session.authUser.ward)
+			}
+		});
+	}
+	console.log(locations)
+	
 	res.render("vwHome/home", { layout: "main", locations });
 });
 
